6984d7ff2a8919bce9873f258628484d0c3d18b8
Kyungpyo Park <toheavener01@gmail.com>
2023-11-24 10:43:35 +0900

    Add readme file and architecture diagram

--- vector_util.py
+++ vector_util.py
@@ -47,7 +47,7 @@ class VectorDatabase():
       try:
         with conn.cursor() as cur:
           cur.execute(f'CREATE SEQUENCE IF NOT EXISTS {table_name}_id_seq AS BIGINT START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1')
-          cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id BIGINT PRIMARY KEY DEFAULT nextval('{table_name}_id_seq'), sql text, description text, parameters text, explore_view varchar(50), model_name varchar(50), desc_vector vector(768))")
+          cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id BIGINT PRIMARY KEY DEFAULT nextval('{table_name}_id_seq'), sql text, description text, parameters text, explore_view varchar(50), model_name varchar(50), table_name varchar(120), column_schema text, desc_vector vector(768))")
       except Exception as e:
         print(e)
         conn.rollback()
@@ -55,12 +55,12 @@ class VectorDatabase():
       conn.commit()
   
   # Insert a row into the table
-  def insert_record(self, sql, parameters, description, explore_view, model_name, desc_vector):
+  def insert_record(self, sql, parameters, description, explore_view, model_name, table_name, column_schema, desc_vector):
     with self.get_connection() as conn:
       try:
         with conn.cursor() as cur:
-          insert_record = (sql, str(description), str(parameters), explore_view, model_name, str(desc_vector).replace(' ',''))
-          cur.execute(f"INSERT INTO rag_test (sql, description, parameters, explore_view, model_name, desc_vector) VALUES ( %s, %s, %s, %s, %s, %s)", insert_record) 
+          insert_record = (sql, str(description), str(parameters), explore_view, model_name, table_name, column_schema, str(desc_vector).replace(' ',''))
+          cur.execute(f"INSERT INTO rag_test (sql, description, parameters, explore_view, model_name, table_name, column_schema, desc_vector) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s)", insert_record) 
       except Exception as e:
         print(e)
         conn.rollback()
@@ -73,7 +73,7 @@ class VectorDatabase():
       try:
         with conn.cursor() as cur:
           select_record = ((desc_vector,))
-          cur.execute(f"SELECT sql, description, parameters, explore_view, model_name FROM rag_test ORDER BY desc_vector <-> %s LIMIT 1", select_record)
+          cur.execute(f"SELECT sql, description, parameters, explore_view, model_name, table_name, column_schema FROM rag_test ORDER BY desc_vector <-> %s LIMIT 1", select_record)
           return cur.fetchone()
       except Exception as e:
         print(e)
@@ -81,3 +81,18 @@ class VectorDatabase():
         raise e
       conn.commit()
 
+  def find_related_tables(self, desc_vector, consine_similarity_threshold):
+    results = []
+    with self.get_connection() as conn:
+      try:
+        with conn.cursor() as cur:
+          select_record = ((desc_vector, consine_similarity_threshold))
+          cur.execute(f"SELECT sql, description, parameters, explore_view, model_name, table_name, column_schema FROM rag_test WHERE (1 - (desc_vector <=> %s)) < %s", select_record)
+          for row in cur.fetchall():
+            results.append(row)
+      except Exception as e:
+        print(e)
+        conn.rollback()
+        raise e
+      conn.commit()
+    return results
